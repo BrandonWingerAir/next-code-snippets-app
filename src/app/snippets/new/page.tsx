f0d763973987669f70bd84f61f10a0f05518c834
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-17 23:38:37 -0400

    new snippet page form and layout styling

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,32 @@
 export default function CreateSnippet() {
-    return <div>
-        Create a Snippet!
-    </div>
+    return <form>
+        <h3 className="font-bold m-3">Create a Snippet</h3>
+        <div className="flex flex-col gap-4">
+            <div className="flex gap-4">
+                <label htmlFor="title" className="w-12">
+                    Title
+                </label>
+                <input 
+                    name="title" 
+                    className="border rounded p-2 w-full" 
+                    id="title"
+                />
+            </div>
+
+            <div className="flex gap-4">
+                <label htmlFor="snippet" className="w-12">
+                    Snippet
+                </label>
+                <textarea 
+                    name="snippet" 
+                    className="border rounded p-2 w-full" 
+                    id="snippet"
+                />
+            </div>
+
+            <button type="submit" className="rounded p-2 bg-blue-200">
+                Save Snippet
+            </button>
+        </div>
+    </form>
 }
