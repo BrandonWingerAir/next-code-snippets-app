10682f5d60728690658c4bf56d13666df939f8d6
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-18 00:07:05 -0400

    save new snippet in database & page redirect

--- prisma/migrations/20241018031241_add_snippet_model/migration.sql
+++ prisma/migrations/20241018031241_add_snippet_model/migration.sql
@@ -2,5 +2,5 @@
 CREATE TABLE "Snippet" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
-    "snippet" TEXT NOT NULL
+    "content" TEXT NOT NULL
 );
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,32 +1,59 @@
-export default function CreateSnippet() {
-    return <form>
-        <h3 className="font-bold m-3">Create a Snippet</h3>
-        <div className="flex flex-col gap-4">
-            <div className="flex gap-4">
-                <label htmlFor="title" className="w-12">
-                    Title
-                </label>
-                <input 
-                    name="title" 
-                    className="border rounded p-2 w-full" 
-                    id="title"
-                />
-            </div>
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
 
-            <div className="flex gap-4">
-                <label htmlFor="snippet" className="w-12">
-                    Snippet
-                </label>
-                <textarea 
-                    name="snippet" 
-                    className="border rounded p-2 w-full" 
-                    id="snippet"
-                />
-            </div>
+export default function CreateSnippetForm() {
+    async function createSnippet(formData: FormData) {
+        // Server Action
+        'use server';
 
-            <button type="submit" className="rounded p-2 bg-blue-200">
-                Save Snippet
-            </button>
-        </div>
-    </form>
+        // Validate user inputs
+        const title = formData.get('title') as string;
+        const content = formData.get('content') as string;
+
+        // Create record in database
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                content
+            }
+        });
+
+        console.log(snippet);
+
+        // Redirect to home route
+        redirect('/');
+    }
+
+    return (
+        <form action={createSnippet}>
+            <h3 className="font-bold m-3">Create a Snippet</h3>
+            <div className="flex flex-col gap-4">
+                <div className="flex gap-4">
+                    <label htmlFor="title" className="w-12">
+                        Title
+                    </label>
+                    <input 
+                        name="title" 
+                        className="border rounded p-2 w-full" 
+                        id="title"
+                    />
+                </div>
+
+                <div className="flex gap-4">
+                    <label htmlFor="content" className="w-12">
+                        Snippet
+                    </label>
+                    <textarea 
+                        name="content" 
+                        className="border rounded p-2 w-full" 
+                        id="content"
+                    />
+                </div>
+
+                <button type="submit" className="rounded p-2 bg-blue-200">
+                    Save Snippet
+                </button>
+            </div>
+        </form>
+    );
 }
